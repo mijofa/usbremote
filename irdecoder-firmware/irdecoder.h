f25ee7c2fc3008f67a893fa29c7ae08f0dface24
= <nik.harris0@gmail.com>
2015-08-25 04:50:27 -0700

    cleanup

--- irdecoder-firmware/irdecoder.h
+++
@@ -1,6 +0,0 @@
-#ifndef _IRDECODER_H_
-#define _IRDECODER_H_
-
-#define USART_BAUD      (((F_CPU / (9600 * 16UL))) - 1)
-
-#endif
--- usb-firmware/usbrequests.h
+++
@@ -1,9 +0,0 @@
-#ifndef _USBREQUESTS_H_
-#define _USBREQUESTS_H_
-
-/* message sent upon connection on host side */
-#define USB_RQ_HELO     0x00
-#define USB_RSP_HELO    0xFF
-
-
-#endif
